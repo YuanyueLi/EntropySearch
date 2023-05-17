325073a231ebb0687b724f6cc23601083dc32639
Yuanyue Li <liyuanyue@gmail.com>
2023-05-17 00:39:56 -0700

    Update build_win.bat

--- build_win.bat
+++ build_win.bat
@@ -1,8 +1,9 @@
 
 call py_env\Scripts\activate.bat
+call pip install -r requirements.txt
 
 cd backend
-call pyinstaller -F main.py
+call pyinstaller -F main.py -n entropy_search_backend.exe
 cd ..
 
 cd frontend
