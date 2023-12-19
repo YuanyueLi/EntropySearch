8a1f706c0b40d115215561d5c4626782f4bd4f51
Yuanyue Li <liyuanyue@gmail.com>
2023-12-18 17:13:30 -0800

    Update.

--- build_win.bat
+++ build_win.bat
@@ -5,7 +5,7 @@ call pip install -r requirements.txt
 cd backend
 rmdir /S /Q dist
 rmdir /S /Q build
-call pyinstaller -F main.py -n entropy_search_backend.exe
+call pyinstaller -F main.py -n flash_entropy_search.exe --add-data ../new_frontend/gui:gui -i icon.ico
 cd ..
 
 cd frontend
